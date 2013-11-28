e71821c77c5baa723e8333b706c358555c7de593
QING Pei <qingpei@sansitech.com>
2013-11-28 13:28:04 +0800

    try to make travis-ci to push to gh-pages

--- ci.sh
+++ ci.sh
@@ -13,3 +13,14 @@ checkerror(){
 
 checkerror
 
+rm gen.log
+
+export REPO="$(pwd | sed s,^/home/travis/builds/,,g)"
+ssh -o StrictHostKeyChecking=no
+git checkout gh-pages
+mv output/site/* ./
+mv output/book.* ./
+git add .
+git commit -m "Update gh-pages after TravisCI build"
+git push https://$(GH_USER):$(GH_PASSWORD)@github.com/${REPO} gh-pages
+
